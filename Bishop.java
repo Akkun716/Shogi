7cd3e50421501b7d6cf7bef302005ae3e2b19624
Akkun716 <adon.anglon@gmail.com>
2024-09-14 04:11:30 -0700

    Updated comments

--- Bishop.java
+++ Bishop.java
@@ -1,3 +1,8 @@
+/***
+ * The shogi bishop moves in a similar fashion to the chess bishop, able to move diagonally
+ * freely as long as there is no piece blocking its path. Once the piece is promoted, it gains
+ * the option to move one square vertically or horizontally.
+ ***/
 public class Bishop extends Piece
 {
     public Bishop(int direction)
@@ -11,17 +16,19 @@ public class Bishop extends Piece
     public Bishop()
     { this(1); }
 
+    // Recall the locations are represented as a 2-digit integer with the 1st int
+    // being the row and the secodn as the column.
     public boolean isValid(int currLocation, int targetLocation)
     {
         int rowDist = currLocation / 10 - targetLocation / 10;
         int colDist = currLocation % 10 - targetLocation % 10;
 
-        /* Promoted bishop movement (regular bishop + one space diagonal) */
+        // Promoted bishop movement (regular + one space horizontal or vertical)
         if(upgrade && (colDist == 0 || rowDist == 0)) {
             return colDist == 0
                 ? rowDist == 1 || rowDist == -1
                 : colDist == 1 || colDist == -1;
-        /* Regular bishop movement */
+        // Regular bishop movement
         } else {
             return rowDist != 0 && colDist != 0
                 ? rowDist / colDist == -1 || rowDist / colDist == 1
