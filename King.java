d9a9b19d9a55e18b93ccde60d4ae9453c592da40
Akkun716 <adon.anglon@gmail.com>
2024-09-18 08:36:54 -0700

    Updated comments

--- King.java
+++ King.java
@@ -1,3 +1,10 @@
+
+/***
+ * Represents a King piece from the Japanese game of Shogi.
+ * 
+ * The shogi king moves in the same fashion to the chess king, able to move one square in any direction.
+ * This piece does not have a promotion option.
+ ***/
 public class King extends Piece
 {
     public King(int direction)
@@ -5,6 +12,7 @@ public class King extends Piece
         this.direction = direction;
         upgrade = true;
         pieceType = "king";
+        // In shogi, the opposing king pieces have different written names.
         if(direction == -1) {
             pieceName = "玉将";
         } else {
@@ -17,10 +25,12 @@ public class King extends Piece
 
     public boolean isValid(int currLocation, int targetLocation)
     {
+        // Recall the locations are represented as a 2-digit integer with the 1st int
+        // being the row and the second as the column.
         int rowDist = currLocation / 10 - targetLocation / 10;
         int colDist = currLocation % 10 - targetLocation % 10;
 
-        /* Regular king movement */
+        // Regular king movement
         return rowDist == 0 && colDist == 0
             ? false
             : colDist >= -1 && colDist <= 1 && rowDist >= -1 && rowDist <= 1;
