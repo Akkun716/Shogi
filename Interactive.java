6ba909c8cc2fd1e32efc3bd5dd07a0be8c1c952a
Akkun716 <adon.anglon@gmail.com>
2024-09-20 11:44:23 -0700

    Updated booleanInput() uses

--- Interactive.java
+++ Interactive.java
@@ -4,26 +4,27 @@ public class Interactive
 
     public static void main(String[] args)
     {
+        int output = 0;
         int currRow = 4;
         int currCol = 4;
         int tarRow = 0;
         int tarCol = 0;
         Board board = new Board();
-        if(USER.trueFalseInput("Fully set up board? [T]rue or [F]alse: ")) {
+        if(USER.booleanInput("Fully set up board?", "True", "False")) {
             board.createDefaultBoard();
         } else {
-            if(USER.trueFalseInput("Default piece location? [T]rue or [F]alse: ")) {
+            if(USER.booleanInput("Default piece location?", "True", "False")) {
             board.createPiece(
                 USER.choiceInput("Select the piece you wish to place: ", board.validPieces()),
                 currRow,
                 currCol, 
-                USER.trueFalseInput("Start as gote (attacking)? [T]rue or [F]alse: "));
+                USER.booleanInput("Start as gote (attacking)?", "True", "False"));
             } else {
                 board.createPiece(
                     USER.choiceInput("Select the piece you wish to place: ", board.validPieces()),
                     USER.intInput("Enter row number: ", 0, board.size()),
                     USER.intInput("Enter column number: ", 0, board.size()),
-                    USER.trueFalseInput("Start as gote (attacking)? [T]rue or [F]alse: "));
+                    USER.booleanInput("Start as gote (attacking)?", "True", "False"));
             }
         }
 
@@ -37,7 +38,7 @@ public class Interactive
                 continue;
             }
             if(!board.getPiece(currRow, currCol).isPromoted()) {
-                if(USER.trueFalseInput("Promote piece? [T]rue or [F]alse: ")) {
+                if(USER.booleanInput("Promote piece?", "True", "False")) {
                     board.promote(currRow, currCol);
                     System.out.printf("%s", board.toString());
                 }
@@ -45,7 +46,9 @@ public class Interactive
             tarRow = USER.intInput("Enter target row number: ", 0, board.size());
             tarCol = USER.intInput("Enter target column number: ", 0, board.size());    
             if(tarRow < 0 || tarCol < 0) { break; }
-            if(board.movePiece(currRow, currCol, tarRow, tarCol)) {
+
+            output = board.movePiece(currRow, currCol, tarRow, tarCol);
+            if(output > 0) {
                 currRow = tarRow;
                 currCol = tarCol;
             }
